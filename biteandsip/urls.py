15bb9eb57a7955110b983205f7accba354d5908e
rockingtushar <agarwaltushar1373@gmail.com>
2025-07-25 23:03:43 +0530

    Add homepage route

--- biteandsip/urls.py
+++ biteandsip/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path("", lambda request: HttpResponse("Bite & Sip API is Live 🚀")),
 ]
